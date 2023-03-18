c48997c6b44d3337ac1958319b27a0f8bd1390d8
Kakhorov Aziz <kaxopob.azizuz@gmail.com>
2023-03-18 09:05:41 +0500

    after Rubocop -a

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails/all"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -13,11 +13,11 @@ module Qna
 
     config.generators do |g|
       g.test_framework :rspec,
-                        request_specs: false,
-                        view_specs: false,
-                        routing_specs: false,
-                        helper_specs: false,
-                        controller_specs: true
+                       request_specs: false,
+                       view_specs: false,
+                       routing_specs: false,
+                       helper_specs: false,
+                       controller_specs: true
     end
   end
 end
