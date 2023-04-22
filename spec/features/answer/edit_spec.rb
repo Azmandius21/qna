1bfab81b84169382cbac642299cc701942942138
Kakhorov Aziz <kaxopob.azizuz@gmail.com>
2023-04-22 13:05:56 +0500

    feat answer edit with not valid data spec

--- spec/features/answer/edit_spec.rb
+++ spec/features/answer/edit_spec.rb
@@ -25,14 +25,37 @@ given!(:question){ create(:question, author: author)}
       click_on 'Edit answer'
       within '.answers' do
         fill_in 'Body', with: 'Edited answer'
-        click_on 'Save'
+        click_on 'Save'        
 
         expect(page).to_not have_content answer.body
         expect(page).to have_content 'Edited answer'
         expect(page).to_not have_selector 'textarea'
       end
     end
-    scenario 'Edit his answer with errors'
-    scenario 'Edit other user answer' 
+
+    scenario 'Edit his answer with errors', js: true do
+      sign_in(author)
+      visit question_path(question)
+
+      click_on 'Edit answer'
+      within '.answers' do
+        fill_in 'Body', with: ''
+        click_on 'Save'        
+
+        expect(page).to have_selector 'textarea'
+      end
+
+      expect(page).to have_content "Body can't be blank"
+    end
+
+    given!(:user){ create(:user) }
+
+    scenario 'Edit other user answer', js: true do
+     
+      sign_in(user)
+      visit question_path(question)
+      save_and_open_page
+      expect(page).to_not have_link 'Edit answer'
+    end
   end
 end
