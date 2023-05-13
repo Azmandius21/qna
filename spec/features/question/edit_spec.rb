e119094e92bffe6620f1c3b731abdbff91a41e10
Kakhorov Aziz <kaxopob.azizuz@gmail.com>
2023-05-13 22:34:20 +0500

    feat: edit question and attach many files

--- spec/features/question/edit_spec.rb
+++ spec/features/question/edit_spec.rb
@@ -25,6 +25,19 @@ feature 'User can edit question ', "
       end
     end
 
+    scenario 'with attaching many files' do
+      visit questions_path
+      click_on 'Edit'
+      fill_in 'question_body', with: 'New question body'
+      fill_in 'question_title', with: 'New question title'
+      
+      attach_file 'Files', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
+      click_on 'Save'
+      
+      expect(page).to have_link 'rails_helper.rb'
+      expect(page).to have_link 'spec_helper.rb'
+    end
+
     scenario 'with invalid data' do
       visit questions_path
       click_on 'Edit'
