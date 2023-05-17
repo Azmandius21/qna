800ce2045a790e4a0d4f4d21d104ecce0b89e78b
Kakhorov Aziz <kaxopob.azizuz@gmail.com>
2023-05-17 23:30:34 +0500

    fix: style for questions , index view

---
+++ app/controllers/attachments_controller.rb
@@ -0,0 +1,5 @@
+class AttachmentsController < ApplicationController
+  def purge
+    
+  end
+end
---
+++ spec/features/purge_attachments_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+require 'byebug'
+
+feature 'purge attachment', "
+  In order to prune attacments in resource 
+  As an author of resource( question, answer)
+  I'd like to be able purge correct attachment
+" do
+  given!(:author){ create(:user) }
+  given!(:question){ create(:question, :with_attached_files, author: author) }
+  
+  describe 'purge attached file to question' do
+    scenario 'authenticated author of question try' do
+      sign_in(author)
+      visit questions_path
+
+      byebug
+      click_on 'Remove file'
+      expect(page).to_not have_link 'test-image.png'
+    end
+    scenario 'authenticated not author try'
+    scenario 'unauthenticated user try'
+  end
+
+  describe 'purge attached file to answer' do
+    scenario 'authenticated author of answer try' 
+    scenario 'authenticated not author try'
+    scenario 'unauthenticated user try'
+  end 
+end
