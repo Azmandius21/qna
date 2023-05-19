5ff3f69733baebcef58a99b343089c5a5617059c
Kakhorov Aziz <kaxopob.azizuz@gmail.com>
2023-05-19 09:41:18 +0500

    fix: changed from redirect to  redirect_back

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -2,6 +2,7 @@ class AttachmentsController < ApplicationController
   def purge
     @attachment = ActiveStorage::Attachment.find(params[:id])
     @attachment.purge
-    redirect_to questions_path
+    
+    redirect_back(fallback_location: root_path)
   end
 end
