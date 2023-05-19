53dea60e5cca6d5b93d041226caa6e3f44888879
Kakhorov Aziz <kaxopob.azizuz@gmail.com>
2023-05-19 10:34:35 +0500

    fix: refactored spec purge_attachments

--- spec/features/purge_attachments_spec.rb
+++ spec/features/purge_attachments_spec.rb
@@ -3,39 +3,68 @@ require 'rails_helper'
 feature 'purge attachment', "
   In order to prune attacments in resource 
   As an author of resource( question, answer)
-  I'd like to be able purge correct attachment
+  I'd like to be able to purge my attachment
 " do
   given!(:author){ create(:user) }
+  given(:not_author){ create(:user) }
   given!(:question){ create(:question, :with_attached_files, author: author) }
   given!(:answer){ create(:answer, :with_attached_files, question: question, author: author)}
   
-  describe 'purge attached file to question' do
-    scenario 'authenticated author of question try' do
-      sign_in(author)
-      visit questions_path
+  describe 'purge attached file' do
+    context 'author tries' do
+      background { sign_in(author) }
 
-      click_on 'Remove file'
-      expect(page).to_not have_link 'test-image.png'
-    end
+      scenario 'to purge attached file on question ' do
+        visit questions_path
 
-    scenario 'authenticated not author try'
-
-    scenario 'unauthenticated user try'
-  end
-
-  describe 'purge attached file to answer' do
-    scenario 'authenticated author of answer try' do
-      sign_in(author)
-      visit question_path(question)
-      #save_and_open_page
-      within '.answers' do
         click_on 'Remove file'
         expect(page).to_not have_link 'test-image.png'
       end
+
+      scenario 'to purge attached file on answer' do 
+        visit question_path(question)
+        
+        within '.answers' do
+          click_on 'Remove file'
+          expect(page).to_not have_link 'test-image.png'
+        end
+      end
     end
 
-    scenario 'authenticated not author try'
-    
-    scenario 'unauthenticated user try'
+    context 'not author tries' do
+      background { sign_in(not_author) }
+
+      scenario 'to purge attached file on question' do
+        visit questions_path
+  
+        expect(page).to_not have_link 'Remove file'
+      end
+
+      scenario 'to purge attached file on answer' do
+        visit question_path(question)
+        
+        within '.answers' do
+          expect(page).to_not have_link 'Remove file'
+        end
+      end
+    end
+
+    context 'unauthenticated user tries' do
+      scenario 'to purge attached file on question' do
+        visit questions_path
+  
+        expect(page).to have_link 'test-image.png'
+        expect(page).to_not have_link 'Remove file'
+      end
+
+      scenario 'to purge attached file on answer' do
+        visit question_path(question)
+  
+        within '.answers' do
+          expect(page).to have_link 'test-image.png'
+          expect(page).to_not have_link 'Remove file'
+        end
+      end
+    end        
   end 
 end
