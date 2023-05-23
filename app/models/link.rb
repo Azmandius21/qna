38fe98b4b1bd897f82485db23e0ca8ebf6996822
Kakhorov Aziz <kaxopob.azizuz@gmail.com>
2023-05-23 19:33:21 +0500

    fix: validates_format_of :url

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,6 +1,7 @@
 class Link < ApplicationRecord
   belongs_to :linkable, polymorphic: true
   
-  validates :name, :url, presence: true,
-            format: { with: URI.regexp, message: 'Invalid format links url'}
+  validates :name, :url, presence: true
+
+  validates_format_of :url, with: URI.regexp, message: 'Invalid format links url'
 end
