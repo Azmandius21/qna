348d91597f3eeb3330b623aea54b03d7c820bd41
Kakhorov Aziz <kaxopob.azizuz@gmail.com>
2023-05-31 15:09:01 +0500

    gist_client waiting integration

--- app/javascript/packs/gist.js
+++ app/javascript/packs/gist.js
@@ -1,18 +1,17 @@
-// $(document).on('turbolinks:load', function(){
-//   $('ul li a').on('click', function(e){
-//    e.preventDefault();
+$(document).on('turbolinks:load', function(){
+  gistLinks = $('ul li .gist-link')
    
-//    // parsing url
-//    const url = new URL(this.href)
-//    const urlPattern = url.username + url.hostname 
-//    const gistId = url.hash
-//    const arr = url.pathname.split('/')
-//    if (urlPattern == 'gist.github.com') {
-//     //$(this).toggle();
-//     console.log('begin')
-//     console.log(arr[2])
-//     console.log('end')
-//    }
-//    //console.log(gistClient)
-//   });
-// })
+   // parsing url
+   const url = new URL(this.href)
+   const urlPattern = url.username + url.hostname 
+   const gistId = url.hash
+   const arr = url.pathname.split('/')
+   if (urlPattern == 'gist.github.com') {
+    //$(this).toggle();
+    console.log('begin')
+    console.log(arr[2])
+    console.log('end')
+   }
+   //console.log(gistClient)
+  });
+})
