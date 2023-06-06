7f2e7091b5496708b33ce2bc2fc632c4d2ac8b4e
Kakhorov Aziz <kaxopob.azizuz@gmail.com>
2023-06-06 22:05:39 +0500

    fix: open gist , but in answers not automatically

--- app/javascript/packs/gist.js
+++ app/javascript/packs/gist.js
@@ -1,29 +1,27 @@
 $(document).on('turbolinks:load', function(){
+  
   let gistLinks = $('.gist-link')
-  const token = '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'
-  console.log(gistLinks) 
+  const token = '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'
 
   const GistClient = require("gist-client")
   const gistClient = new GistClient()
   gistClient.setToken(token)
   jQuery.each(gistLinks, function(){
     const gistId = $(this).data('gist-id');
-    gistClient.getOneById(gistId)
-    //alert($(this).data('url'));
+    gistClient.getOneById(gistId).then(
+     Gist => {
+      const gist = Gist
+      const gistFile = gist['files']['gistfile1.txt']
+      const gistFileName = gist['files']['gistfile1.txt']['filename']
+      const content = gistFile['content']
+      const owner = gist['owner']['login']
+     
+      $(this).append("<div class='gist'>\
+                        <div class='title'>Title:"+ Gist.description +"</div>\
+                        <div class='file-name'>File name:"+ gistFileName +"</div>\
+                        <div class='context'>Content:"+ content +"</div>\
+                        <div class='owner'>Owner:"+ owner +"</div>\
+                     </div>")     
+    })
   })
 })
-
-
-
-
-// parsing url
-//  const url = new URL(this.href)
-//  const urlPattern = url.username + url.hostname 
-//  const gistId = url.hash
-//  const arr = url.pathname.split('/')
-//  if (urlPattern == 'gist.github.com') {
- //$(this).toggle();
- // console.log(arr[2])
- // console.log('end')
-
-//console.log(gistClient)
