029372da67b8bf95d53203d9968429ddb1281cd1
Kakhorov Aziz <kaxopob.azizuz@gmail.com>
2023-06-13 11:09:26 +0500

    create reward model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_23_045258) do
+ActiveRecord::Schema.define(version: 2023_06_13_054434) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,12 @@ ActiveRecord::Schema.define(version: 2023_05_23_045258) do
     t.bigint "best_answer_id"
     t.index ["author_id"], name: "index_questions_on_author_id"
     t.index ["best_answer_id"], name: "index_questions_on_best_answer_id"
+  end
+
+  create_table "rewards", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
