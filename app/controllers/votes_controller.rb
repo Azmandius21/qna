24f0bb405d5cd1a47ce131b33b5a14588b3fc2d8
Kakhorov Aziz <kaxopob.azizuz@gmail.com>
2023-07-02 02:23:04 +0500

    spec votes_controller

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,5 +1,6 @@
+require 'byebug'
 class VotesController < ApplicationController
-  before_action :authenticate_user!
+   before_action :authenticate_user!
 
   def create
     @question = Question.find(params[:question_id])
