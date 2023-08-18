263c689d8d8008509dda5089f6fe68dde55b18cb
Kakhorov Aziz <kaxopob.azizuz@gmail.com>
2023-08-18 21:21:04 +0500

    feat: add voted concern to question controller

--- app/models/concerns/votable.rb
+++ app/models/concerns/votable.rb
@@ -2,7 +2,7 @@ module Votable
   extend ActiveSupport::Concern
 
   included do
-    has_many :votes, as: :votable
+    has_many :votes, dependent: :destroy, as: :votable
   end
 
   def detect_route_path
@@ -15,5 +15,13 @@ module Votable
     attr_id = params.keys.select{ |key| key if key =~ /_id$/ }.first
     votable = attr_id.split('_')[0].capitalize.constantize
     @votable = votable.find(params[attr_id].to_i)
+  end
+
+  def voted_by?(user)
+    self.votes.where(user_id: user.id) || false
+  end
+
+  def vote_sum
+    self.votes.where(liked: true).count - self.votes.where(liked: false).count
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  concern :votable do
+    member do
+      patch 'like'
+      patch 'dislike'
+      patch 'reset'
+      
+    end
+  end
+
   get 'user/:id', to: 'users#show_rewards', as: 'user_show_rewards'
 
-  resources :questions do
+  resources :questions, concerns: %i[ votable] do
     resources :answers, shallow: true do
       patch 'select', on: :member
     end
@@ -11,9 +20,9 @@ Rails.application.routes.draw do
 
   delete 'attachments/:id/purge', to: 'attachments#purge', as: 'purge_attachment'
 
-  post   'questions/:question_id/votes/', to: 'votes#create', as: 'create_vote_to_question'
-  post   'answers/:answer_id/votes/', to: 'votes#create', as: 'create_vote_to_answer'
-  delete 'votes/:id/destroy', to: 'votes#destroy', as: 'destroy_vote'
+  # post   'questions/:question_id/votes/', to: 'votes#create', as: 'create_vote_to_question'
+  # post   'answers/:answer_id/votes/', to: 'votes#create', as: 'create_vote_to_answer'
+  # delete 'votes/:id/destroy', to: 'votes#destroy', as: 'destroy_vote'
 
   root to: 'questions#index'
 
