18be6f8daa13f5c53411990193c0551fd207497e
Kakhorov Aziz <kaxopob.azizuz@gmail.com>
2023-08-23 12:04:43 +0500

    add bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,11 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "stylesheets/application"
+
+import "bootstrap/dist/js/bootstrap"
+import "bootstrap/dist/css/bootstrap"
+
 
 require("jquery")
 require("@nathanvda/cocoon")
