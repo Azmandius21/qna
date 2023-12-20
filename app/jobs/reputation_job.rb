f1f2a9bd91213a1c516a75fdab6a6025096a0859
Kakhorov Aziz <kaxopob.azizuz@gmail.com>
2023-12-20 10:52:27 +0500

    deleted: reputation servise and job

--- app/jobs/reputation_job.rb
+++
@@ -1,7 +0,0 @@
-class ReputationJob < ApplicationJob
-  queue_as :default
-
-  def perform(object)
-    Calculation::Reputation.calculate(object)
-  end
-end
--- app/services/calculation/reputation.rb
+++
@@ -1,6 +0,0 @@
-class Calculation::Reputation
-  def self.calculate(obj)
-    #....
-    sleep(3)
-  end
-end
