b953d7f4884c99df43b3f7b2081ba4d854c1fc81
Kakhorov Aziz <kaxopob.azizuz@gmail.com>
2023-12-26 09:43:44 +0500

    fix: if new_question is  not present the daily digest will not be sent

--- app/services/mail_services/daily_digest.rb
+++ app/services/mail_services/daily_digest.rb
@@ -2,8 +2,10 @@ class MailServices::DailyDigest
   def send_digest
     @new_questions = Question.last_day_questions.map { |q| q }
 
-    User.find_each(batch_size: 500) do |user|
-      DailyDigestMailer.digest(user, @new_questions).deliver_later
+    if @new_questions
+      User.find_each(batch_size: 500) do |user|
+        DailyDigestMailer.digest(user, @new_questions).deliver_later
+      end
     end
   end
 end
