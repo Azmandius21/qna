08c7bebb310ece26eb023c674519dea29ac28835
Kakhorov Aziz <kaxopob.azizuz@gmail.com>
2023-12-30 21:53:02 +0500

    fix: template search and question_spec

--- spec/features/thinking_sphinx/questions_spec.rb
+++ spec/features/thinking_sphinx/questions_spec.rb
@@ -5,47 +5,40 @@ feature 'User can search for question', "
   I'd like to be able to search for the question
 " do
   describe 'User search for the question', js: true do
-    given(:author) { create(:user, email: 'question@test.ru') }
-    given(:question1) do
-      create(:question, title: 'find in body', body: 'question body')
-    end
-    given(:question2) do
-      create(:question, title: 'question title', body: 'find in title')
-    end
-    given(:question3) do
-      create(:question, author: author, title: 'title3', body: 'find in author email')
-    end
-  end
+    given!(:author) { create(:user, email: 'question@test.ru') }
+    given!(:question1) { create(:question, title: 'find in body', body: 'question body') }
+    given!(:question2) { create(:question, title: 'question title', body: 'find in title') }
+    given!(:question3){ create(:question, author: author, title: 'title3', body: 'find in author email')}
 
-  scenario 'data present', sphinx: true do
-    ThinkingSphinx::Test.run do
-      visit questions_path
-      within '.search' do
-        fill_in 'query', with: 'question'
-        select_by_value 'search_area', 'question'
-        click_on 'Find'
-      end
+    background { visit questions_path }
 
-      within '.result-search' do
-        expect(page).to have_content 'find in body'
-        expect(page).to have_content 'find in title'
-        expect(page).to have_content 'find in author email'
+    scenario 'Content has query items', sphinx: true do
+      ThinkingSphinx::Test.run do
+        within '.search' do
+          fill_in 'query', with: 'question'
+          select_by_value 'search_area', 'question'
+          click_on 'Find'
+        end
+
+        within '.result-search' do
+          expect(page).to have_content 'find in body'
+          expect(page).to have_content 'find in title'
+          expect(page).to have_content 'find in author email'
+        end
       end
     end
-  end
 
-  scenario 'empty result', sphinx: true do
-    ThinkingSphinx::Test.run do
-      visit questions_path
+    scenario 'Content has not query items', sphinx: true do
+      ThinkingSphinx::Test.run do
+        within '.search' do
+          fill_in 'query', with: 'empty'
+          select_by_value 'search_area', 'question'
+          click_on 'Find'
+        end
 
-      within '.search' do
-        fill_in 'query', with: 'empty'
-        select_by_value 'search_area', 'question'
-        click_on 'Find'
-      end
-
-      within '.result-search' do
-        expect(page).to have_content 'No items found'
+        within '.result-search' do
+          expect(page).to have_content 'No items found'
+        end
       end
     end
   end
