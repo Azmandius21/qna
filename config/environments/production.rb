5ba8ca1deb0fa695ed3fd9fc136174ad39a85433
Kakhorov Aziz <kaxopob.azizuz@gmail.com>
2024-01-18 09:58:27 +0500

    Add sets for action_mailer in production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,8 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "qna_production"
 
   config.action_mailer.perform_caching = false
-
+  config.action_mailer.default_url_options = { host: '79.174.95.43' }
+  config.action_mailer.delivery_method = :smtp
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
