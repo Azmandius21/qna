91072ebdd8e08b0d0cad1c2f5f87ce993df3b757
Kakhorov Aziz <kaxopob.azizuz@gmail.com>
2024-01-19 12:41:35 +0500

    add smtp setting and edited credentials: add gmail_password

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,8 +65,18 @@ Rails.application.configure do
   # Mailer settings
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: '79.174.95.43', port: 80 }
-  config.action_mailer.default_options = { from: "kaxopob.azizuz@gmail.com", reply_to: "kaxopob.azizuz@gmail.com"}
+  config.action_mailer.default_options = { from: "azizqna3@gmail.com", reply_to: "azizqna3@gmail.com"}
   config.action_mailer.delivery_method = :smtp
+
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => "aziz qna",
+    :password             => Rails.application.credentials[:gmail_password],
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
 
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
